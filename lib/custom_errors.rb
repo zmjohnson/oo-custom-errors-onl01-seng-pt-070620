208033c17f9e067b05ce7d59f6a9830406b8393f
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-08-08 01:34:44 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,8 +7,16 @@ class Person
 
   def get_married(person)
     self.partner = person
-    person.partner = self
-  end
+    if person.class != Person
+      begin
+        raise PartnerError
+      rescue PartnerError => error
+          puts error.message
+      end
+    else
+      person.partner = self
+    end
+end
 
 end
 
